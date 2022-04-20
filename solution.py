b59bd8728947fcb6da2b2c35d6fee18786565469
kj33305 <98822946+kj33305@users.noreply.github.com>
2022-04-20 18:29:22 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -87,26 +87,26 @@ def get_route(hostname):
                 whatReady = select.select([mySocket], [], [], timeLeft)
                 howLongInSelect = (time.time() - startedSelect)
                 if whatReady[0] == []: # Timeout
-                    tracelist1.append("* * * Request timed out.")
+                    tracelist1.append("Request timed out.")
                     #Fill in start
                     #You should add the list above to your all traces list
                     tracelist2.append([str(ttl), tracelist1[-1]])
 
                     tracelist1.clear()
-                    #print("* * * Request timed out.")
+                    #print("Request timed out.")
                     # print('test')
                     #Fill in end
                 recvPacket, addr = mySocket.recvfrom(1024)
                 timeReceived = time.time()
                 timeLeft = timeLeft - howLongInSelect
                 if timeLeft <= 0:
-                    tracelist1.append("* * * Request timed out.")
+                    tracelist1.append("Request timed out.")
                     #Fill in start
                     #You should add the list above to your all traces list
                     tracelist2.append([str(ttl), tracelist1[-1]])
 
                     tracelist1.clear()
-                    #print("* * * Request timed out.")
+                    #print("Request timed out.")
                     # print('test)')
                     #Fill in end
             except timeout:
